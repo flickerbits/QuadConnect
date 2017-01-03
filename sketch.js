f0da646a5c548717fa27b427e9dcdea231f8f50a
Hugh <hughsuh@gmail.com>
2017-01-03 13:18:49 -0600

    change board to 2D array

--- sketch.js
+++ sketch.js
@@ -1,25 +1,28 @@
-var board = [];
+var columns = [];
 
 function setup() {
   createCanvas(500, 500);
   var x = 40;
   var y = 450;
 
-  for (var i = 1; i < 9; i++) {
+  for (var i = 0; i < 8; i++) {
+    columns[i]= [];
     for (var j = 0; j < 6; j++) {
-      var gamePiece = new GamePiece(x, y - (j * 60))
-      ellipse(x, y - (j * 60), 50, 50)
-      board.push(gamePiece);
+      var gamePiece = new GamePiece(x, y - (j * 60), 0);
+      ellipse(x, y - (j * 60), 50, 50);
+      columns[i].push(gamePiece);
     }
     x += 60;
   }
-  // console.log(board)
+  console.log(columns);
 }
 
 function draw() {
   background(0, 0, 200);
-  for (var i = 0; i < board.length; i++) {
-    board[i].show()
+  for (var i = 0; i < columns.length; i++) {
+    for (var j = 0; j < columns[i].length; j++) {
+      columns[i][j].show();
+    }
   }
 }
 
