fd3ab98849a2d35b3e8d9bb02add3aa891762b9b
Hugh <hughsuh@gmail.com>
2017-01-03 01:48:05 -0600

    fix board to 8 columns wide

--- gamePiece.js
+++ gamePiece.js
@@ -3,7 +3,7 @@ function GamePiece(x, y, player) {
   this.y = y;
 
   this.show  = function() {
-    fill(225);
+    if (player === 0) { fill(225) };
     if (player === 1) { fill(255, 0, 0) };
     if (player === 2) { fill(255, 255, 0) };
     ellipse(this.x, this.y, 50, 50);
