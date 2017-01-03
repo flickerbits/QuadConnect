35cf53eedfaba96a5dceea4d1025fae5963c4296
Hugh <hughsuh@gmail.com>
2017-01-03 13:50:59 -0600

    add currentPiece and move functionality

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
 var columns = [];
+var currentPlayer = 1;
+var currentPiece;
+var spacing = 60;
 
 function setup() {
   createCanvas(500, 500);
   var x = 40;
-  var y = 450;
+  var y = 455;
 
   for (var i = 0; i < 8; i++) {
     columns[i]= [];
     for (var j = 0; j < 6; j++) {
-      var gamePiece = new GamePiece(x, y - (j * 60), 0);
-      ellipse(x, y - (j * 60), 50, 50);
+      var gamePiece = new GamePiece(x, y - (j * spacing), 0);
+      ellipse(x, y - (j * spacing), 50, 50);
       columns[i].push(gamePiece);
     }
-    x += 60;
+    x += spacing;
   }
   console.log(columns);
+  currentPiece = new GamePiece(40, 95, currentPlayer);
 }
 
 function draw() {
   background(0, 0, 200);
+  currentPiece.show();
+
   for (var i = 0; i < columns.length; i++) {
     for (var j = 0; j < columns[i].length; j++) {
       columns[i][j].show();
@@ -26,3 +32,12 @@ function draw() {
   }
 }
 
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    currentPiece.move(1);
+  } else if (keyCode === LEFT_ARROW) {
+    currentPiece.move(-1);
+  } else if (keyCode === DOWN_ARROW) {
+
+  }
+}
