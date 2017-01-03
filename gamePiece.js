a48f0238294809c238778ab8ec8535b0e197a584
Hugh <hughsuh@gmail.com>
2017-01-03 01:51:36 -0600

    gamepiece colors

--- gamePiece.js
+++ gamePiece.js
@@ -3,8 +3,11 @@ function GamePiece(x, y, player) {
   this.y = y;
 
   this.show  = function() {
+    // empty space GRAY
     if (player === 0) { fill(225) };
+    // player 1 RED
     if (player === 1) { fill(255, 0, 0) };
+    // player 2 YELLOW
     if (player === 2) { fill(255, 255, 0) };
     ellipse(this.x, this.y, 50, 50);
   }
