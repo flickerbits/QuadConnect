c044c6862a1e79382ab505c20840225eb8b251da
Hugh <hughsuh@gmail.com>
2017-01-04 13:14:51 -0600

    optimize diagonal backslash check

--- checkWinner.js
+++ checkWinner.js
@@ -11,17 +11,17 @@ function checkWinner(col, row) {
 
   // check current row for winner
   streak = 0;
-  for (var j = 0; j < 8; j++) {
+  for (var j = 0; j < 7; j++) {
     if (columns[j][row].color === currentPlayer) {
       streak++;
       if (streak === 4) return true;
     } else streak = 0;
   }
 
-  // check diagonal forwardslash that intersects location
+  // check only diagonal forwardslash that intersects location
   streak = 0;
   var base = col - row;
-  if (base > 0 && base < 5) {
+  if (base > 0 && base < 4) {
     for (var k = 0; k < 6; k++) {
       if (columns[base + k] && columns[base + k][k].color === currentPlayer) {
         streak++;
@@ -41,25 +41,15 @@ function checkWinner(col, row) {
     }
   }
 
-  // check diagonal backslash that intersects location
+  // check only diagonal backslash that intersects location
   streak = 0;
   base = col + row;
-  if (base > 2 && base < 8) {
+  if (base > 2 && base < 9) {
     for (var m = 0; m < 6; m++) {
       if (columns[base - m] && columns[base - m][m].color === currentPlayer) {
         streak++;
         if (streak === 4) return true;
       } else streak = 0;
-    }
-  } else {
-    streak = 0;
-    if (base > 7 && base < 10) {
-      for (var n = 0; n < 6; n++) {
-        if (columns[base - n] && columns[base - n][n].color === currentPlayer) {
-          streak++;
-          if (streak === 4) return true
-        } else streak = 0;
-      }
     }
   }
   return false;
