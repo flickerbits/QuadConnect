126fa89b4ef77b030649d1b1b7a53e25ace126f0
Hugh <hughsuh@gmail.com>
2017-01-03 19:43:46 -0600

    work in progress- check diagonals

--- checkWinner.js
+++ checkWinner.js
@@ -10,5 +10,40 @@ function checkWinner(col, row) {
     } else streak = 0;
   }
 
+  // check current row for winner
+  streak = 0;
+  for (var j = 0; j < 8; j++) {
+    if (columns[j][row].color === currentPlayer) {
+      streak++;
+      if(streak === 4) return true;
+    } else streak = 0;
+  }
+
+  // check diagonal forward slash
+  streak = 0;
+  // check col values > 0
+  var base = col - row;
+  if (base > 0 && base < 5) {
+    for (var k = 0; k < 6; k++) {
+      console.log('checking:', k + base, k);
+      if (columns[base + k][k].color === currentPlayer) {
+        streak++;
+        if(streak === 4) return true;
+      } else streak = 0;
+    }
+  } else {
+    // check row values > 0
+    base = row - col;
+    if (base >= 0 && base < 3) {
+      for (var l = 0; l < 6 - base; l++) {
+        console.log('checking:', l , base + l);
+        if (columns[l][base + l].color === currentPlayer) {
+          streak++;
+          if(streak === 4) return true
+        } else streak = 0;
+      }
+    }
+  }
+
   return false;
 }
