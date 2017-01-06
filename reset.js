cca262782ce841636418d742e6da4d26346e78e4
Hugh <hughsuh@gmail.com>
2017-01-06 17:54:14 -0600

    set winner to false when reset is called

--- reset.js
+++ reset.js
@@ -1,8 +1,9 @@
 function reset() {
   columns = [];
   bg = {'r':0, 'g':0, 'b':200};
-  var x = 40;
-  var y = 455;
+  winner = false;
+  x = 40;
+  y = 455;
 
   // fill columns with 'empty' gray game pieces to initialize game
   for (var i = 0; i < 7; i++) {
