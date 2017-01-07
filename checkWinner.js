88b6600f0ff48a5309c5c63d0c5c140cdad45709
flickerbits <hughsuh@gmail.com>
2017-01-07 17:17:49 -0600

    rewrite column check with winArray

--- checkWinner.js
+++ checkWinner.js
@@ -1,27 +1,29 @@
 function checkWinner(col, row) {
   var winStroke = 5;
+  var winArray = [];
+  console.log("CurrentPiece:", col, row)
   
   // check current COLUMN for winner
-  var streak = 0;
   for (var i = 0; i < 6; i++) {
     if (columns[col][i].color === currentPlayer) {
-      streak++;
-      columns[col][i].stroke = winStroke;
-      if (streak === 4) return true;
-    } else {
-      streak = 0; 
-      for (var r = 0; r <= i; r++) {
-        columns[col][r].stroke = 1;
+      winArray.push(columns[col][i]);
+      if (winArray.length === 4) {
+        winArray.forEach(function(e) {
+          e.stroke = 4;
+        });
+        return true;
       }
+    } else {
+      winArray = [];
     }
   }
 
   // check current ROW for winner
-  streak = 0;
+  var streak = 0;
   for (var j = 0; j < 7; j++) {
     if (columns[j][row].color === currentPlayer) {
       streak++;
-      columns[j][row].stroke = winStroke;
+      // columns[j][row].stroke = winStroke;
       if (streak === 4) return true;
     } else {
       streak = 0;
@@ -29,33 +31,46 @@ function checkWinner(col, row) {
         columns[r][row].stroke = 1;
       }
     }
-  };
+  }
+  for (var r = 0; r < 7; r++) {
+    columns[r][row].stroke = 1;
+  }
 
   // check only diagonal Forwardslash that intersects location
   streak = 0;
   var base = col - row;
-  if (base > 0 && base < 4) {
+  if (base >= 0 && base < 4) {
     for (var k = 0; k < 6; k++) {
       if (columns[base + k] && columns[base + k][k].color === currentPlayer) {
         streak++;
-        columns[base + k][k].stroke = winStroke;
+        // columns[base + k][k].stroke = winStroke;
         if (streak === 4) return true;
-      } else streak = 0;
+      } else {
+        streak = 0;
+        for (var r = 0; r < k + 1; r++) {
+          if (columns[base + r]) {
+            columns[base + r][r].stroke = 1;
+          }
+        }
+      }
     }
-    // for (var k = 0; k < 6; k++) {
-    //   if (columns[base + k]) {
-    //     columns[base + k][k].stroke = 1;
-    //   }
-    // }
   } else {
     streak = 0;
     base = row - col;
-    if (base >= 0 && base < 3) {
+    if (base > 0 && base < 3) {
       for (var l = 0; l < 6; l++) {
         if (columns[l][base + l] && columns[l][base + l].color === currentPlayer) {
           streak++;
+          // columns[l][base + l].stroke = winStroke;
           if (streak === 4) return true;
-        } else streak = 0;
+        } else {
+          streak = 0;
+          for (var r = 0; r < l; r++) {
+            if (columns[base + r]) {
+              columns[base + r][r].stroke = 1;
+            }
+          }
+        }
       }
     }
   }
@@ -67,15 +82,19 @@ function checkWinner(col, row) {
     for (var m = 0; m < 6; m++) {
       if (columns[base - m] && columns[base - m][m].color === currentPlayer) {
         streak++;
-        columns[base - m][m].stroke = winStroke;
+        // columns[base - m][m].stroke = winStroke;
         if (streak === 4) return true;
       } else streak = 0;
     }
-    // for (var m = 0; m < 6; m++) {
-    //   if (columns[base - m]) {
-    //     columns[base - m][m].stroke = 1;
-    //   }
-    // }
+    for (var m = 0; m < 6; m++) {
+      if (columns[base - m]) {
+        columns[base - m][m].stroke = 1;
+      }
+    }
   }
   return false;
+}
+
+function resetStroke(col, row){
+  columns[col][row].stroke = 1;
 }
